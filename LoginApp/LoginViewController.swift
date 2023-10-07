aac1f5b37f46d4967b4266df0e18df864b5ce5c2
Kirill Satalkin <baboch@mail.ru>
2023-10-08 02:29:34 +0300

    setup log

--- LoginApp/LoginViewController.swift
+++ LoginApp/LoginViewController.swift
@@ -8,22 +8,21 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
+    
+    
+    @IBOutlet weak var labelWelcome: UILabel!
+    
+    var userName: String?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        if let userName = userName {
+            labelWelcome.text = "Welcome, \(userName)!"
+            
+        }
+        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- LoginApp/ViewController.swift
+++ LoginApp/ViewController.swift
@@ -8,12 +8,88 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var userNameTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        customizingTextFields()
+        
+        
     }
-
-
+    
+    //осуществление входа
+    var user = "User"
+    var password = "Password"
+    
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        guard userNameTextField.text == user, passwordTextField.text == password else {
+            showAlert()
+            return false
+        }
+        return true
+    }
+    
+    //передача имени на второй экран
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "LoginViewController" {
+            if let destinationViewController = segue.destination as? LoginViewController {
+                destinationViewController.userName = userNameTextField.text
+            }
+        }
+    }
+    //скрытие клавиатуры
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+        super.touchesBegan(touches, with: event)
+    }
+    
+    //возврат на первое окно и удаление строк
+    @IBAction func unwind(for sender: UIStoryboardSegue){
+        userNameTextField.text = ""
+        passwordTextField.text = ""
+    }
+    
+    // подсказки user и password
+    @IBAction func showAlertPasswordAction(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Oops!", message: "Your password is ~Password~ 😉", preferredStyle: .alert)
+        let okButton = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(okButton)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func showAlertLoginAction(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Oops!", message: "Your user name is ~User~😉", preferredStyle: .alert)
+        let okButton = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(okButton)
+        
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+    // алерт об ошибке ввода
+    private func showAlert() {
+        let alert = UIAlertController(title: "Invalid login or password 🥺", message: "Please, enter correct login and password", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        userNameTextField.text = ""
+        passwordTextField.text = ""
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
+        
+    }
+    //настройка клавиатуры
+    private func customizingTextFields () {
+        passwordTextField.autocorrectionType = .no
+        passwordTextField.smartQuotesType = .no
+        passwordTextField.isSecureTextEntry = true
+        
+        userNameTextField.autocorrectionType = .no
+        userNameTextField.smartQuotesType = .no
+    }
+    
 }
-
